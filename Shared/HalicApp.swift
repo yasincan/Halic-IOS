fe032913032ef319df3421b7fcfd23b058be01ab
Yasin Can <yasincan@Yasin-MacBook-Air.local>
2021-06-23 23:11:13 +0300

    init commit

---
+++ Shared/HalicApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalicApp.swift
+//  Shared
+//
+//  Created by Yasin Can on 19.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HalicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
